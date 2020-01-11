7eb53e34fd5ea3c872055d7b075a10d2d0e8f4ad
w1nst0n <mreve@keemail.me>
2020-01-11 19:23:27 +0100

    Moved Verizon Motorola specific packages to Carriers.sh

--- lists/Motorola.sh
+++ lists/Motorola.sh
@@ -1,10 +1,4 @@
+#!/bin/bash
+
 declare -a motorola_bloat=(
-	"com.motorola.mot5gmod"
-	"com.motorola.vzw.mot5gmod"
-	"com.discoveryscreen"
-	"com.motorola.vzw.cloudsetup"
-	"com.motorola.ltebroadcastservices_vzw"
-	"com.motorola.vzw.loader"
-	"com.motorola.omadm.vzw"
-	"com.motorola.vzw.provider"
 	)
