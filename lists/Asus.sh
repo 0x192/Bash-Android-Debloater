a406e1cc9ebd8ac7157011f8efaade93870572bf
w1nst0n <mreve@keemail.me>
2020-01-15 14:55:56 +0100

    Added ASUS documentation

--- lists/Asus.sh
+++ lists/Asus.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 declare -a asus_bloat=(
-	"com.asus.soundrecorder"                     
-	"com.caf.fmradio" #Radio app (Transistor [https://f-droid.org/en/packages/org.y20k.transistor/] is much better)
-	"com.huaqin.FM"                                                  
-	"com.generalmobi.go2pay"                                
-	"com.asus.ia.asusapp"                        
-	"id.co.babe"                                 
-	"com.pure.indosat.care"                      
-	"com.shopee.id"                                                        
-	#******** DEBLOAT AVANCE *******#
-	#"com.asus.calculator"                        
+
+	"com.asus.calculator"
+	# Asus calculator app
+	# NOTE : Simple calculator is a good alternative : https://f-droid.org/en/packages/com.simplemobiletools.calculator/
+
+	"com.asus.ia.asusapp"
+	# My Asus (https://play.google.com/store/apps/details?id=com.asus.ia.asusapp)
+	# Asus service center (support + store)              
+
+	"com.asus.soundrecorder"
+	# Asus Sound recorder (https://play.google.com/store/apps/details?id=com.asus.soundrecorder)                         
 	)
