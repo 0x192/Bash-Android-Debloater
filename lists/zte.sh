a0b299fa845266a51b50a486c2e4340dbac5d6d7
w1nst0n <mreve@keemail.me>
2020-03-17 16:21:41 +0100

    zte support

---
+++ lists/zte.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# I NEVER HAD A LG DEVICE ON HAND. 
+# I did some intensive searches on the web to find a list and I try my best to document it. But I need LG users to really improve it.
+# I use [MORE INFO NEEDED] tag as a marker.
+
+declare -a zte=(
+
+	"com.zte.assistant"
+	# ZTE Voice Assistant
+
+	"com.zte.weather"
+	# ZTE Weather app
+	)
