5f6e5b580c3531306bd97344280eee853bf97097
w1nst0n <mreve@keemail.me>
2020-06-22 18:38:32 +0200

    Fixed bad array name (#7)

--- lists/LG.sh
+++ lists/LG.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-declare -a lg=(
+declare -a lge=(
 	# I NEVER HAD A LG DEVICE ON HAND. 
 	# I did some intensive searches on the web to find a list and I try my best to document it. But I need LG users to really improve it.
 	# I use [MORE INFO NEEDED] tag as a marker.
