f09dfe8fe7871c3d1cab0717e8b841fd3484526f
w1nst0n <mreve@keemail.me>
2021-03-05 23:24:28 +0100

    Improve documentation

--- lists/Asus.sh
+++ lists/Asus.sh
@@ -8,6 +8,7 @@ declare -a asus=(
 
     "com.asus.easylauncher"  
     # Asus Easy Mode (https://play.google.com/store/apps/details?id=com.asus.easylauncher)
+    # Alternative launcher with bigger icons and simpler interface
 
 	"com.asus.ia.asusapp"
 	# My Asus (https://play.google.com/store/apps/details?id=com.asus.ia.asusapp)
@@ -17,5 +18,8 @@ declare -a asus=(
 	# Asus Sound recorder (https://play.google.com/store/apps/details?id=com.asus.soundrecorder) 
 
     "com.asus.userfeedback"
-    # Asus ZenPad Care                        
+    # ZenUI Help (https://play.google.com/store/apps/details?id=com.asus.userfeedback)
+    # Customer service app that provides FAQs, Mobile care service, user feedback, and public forums.
+    # Lots of telemetry (insecure on top of that):
+    # https://beta.pithus.org/report/e80a1fa70adc097fc9817720b5c8c81cfd156a76e6d062759b2bc3d6937a97e7          
 	)
